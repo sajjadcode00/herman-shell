4169265e7667e9f0b771700bcf1c658e4d660e3e
sajjad <iamnull.sajjad@gmail.com>
2025-07-27 20:38:13 +0330

    ready for develop

--- herman.c
+++ herman.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 
+#define MAX_ARGS 64
+
 char* read_command(){
 	printf("herman> ");
 	char* line = NULL;
@@ -11,12 +13,24 @@ char* read_command(){
 	return line;
 	free(line);
 }
+
+char** parse_command(char *line) {
+	char **args = malloc(sizeof(char*) * MAX_ARGS);
+	int i = 0;
+	char *token = strtok(line, " \t\n");
+	while (token != NULL) {
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+}
+
+
 int main() 
 {
 	while (1) {
 		char* line = read_command();	
-		printf ("You enreed: %s", line);
-		line[strcspn(line, "\n")] = '\0';	
+		printf ("You entered: %s", line);	
 	}
 	return 0;
 }
