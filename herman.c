71660b9bbddf989d8d70174c196d1cc4ba6a0d24
sajjad <iamnull.sajjad@gmail.com>
2025-07-27 13:22:27 +0330

    begin of hopeless

--- herman.c
+++ herman.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <string.h>
 
+
+char* read_command(){
+	printf("herman> ");
+	char* line = NULL;
+	size_t len = 0;
+	getline(&line, &len, stdin);
+	return line;
+	free(line);
+}
 int main() 
 {
-	while(1) {
-		printf("herman> ");
-		char* line = NULL;
-		size_t len = 0;
-		getline(&line, &len, stdin);
-		printf("You entered: %s", line);
-		free(line);
+	while (1) {
+		char* line = read_command();	
+		printf ("You enreed: %s", line);
+		line[strcspn(line, "\n")] = '\0';	
 	}
-
 	return 0;
 }
