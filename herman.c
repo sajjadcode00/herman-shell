6a8c3b6b3d8cff2d29f237db2d4780a553cf8493
sajjad <iamnull.sajjad@gmail.com>
2025-07-29 22:07:56 +0330

    pwd added

--- herman.c
+++ herman.c
@@ -9,7 +9,7 @@
 
 #define MAX_ARGS 64
 
-char* read_command() {
+char* read_command() { // read command from shell
 	printf("herman> ");
 	fflush(stdout);
 	char* line = NULL;
@@ -19,7 +19,7 @@ char* read_command() {
 	free(line);
 }
 
-char** parse_command(char *line) {
+char** parse_command(char *line) { // manage command on arry for use in execvp
 	char **args = malloc(sizeof(char*) * MAX_ARGS);
 	int i = 0;
 	char *token = strtok(line, " \t\n");
@@ -38,8 +38,18 @@ int main()
 		//printf ("You entered: %s", line);
 		char **my_command = parse_command(line);
 
+		if (strcmp(my_command[0], "pwd") == 0) { //for pwd. actually this diff between addres of my_command and pwd
+			char cwd[1024];
+			if (getcwd(cwd, sizeof(cwd)) != NULL) { //this func give us current path 
+				printf("%s\n", cwd);
+			} else {
+				perror("getcwd error\n");
+			}
+			continue;
+		}
+
 		int fd[2];
-		if (pipe(fd) == -1) {
+		if (pipe(fd) == -1) { //this is for ls
 			perror("pipe\n");
 			exit(EXIT_FAILURE);
 		}
@@ -59,7 +69,7 @@ int main()
 			execvp(my_command[0], my_command);
 			perror("exec failed\n");
 			exit(EXIT_FAILURE);
-		}else {
+		} else {
 			//parent procces
 			close(fd[1]);
 			char buffer[4096];
@@ -71,7 +81,7 @@ int main()
 
 			if (n == 0) {printf("There is no file or directory\n");}
 			else {printf("%s", buffer);}
-		}
+	       	}
 
 	}
 
